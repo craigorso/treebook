c1bee30b594f2c9b6a8dce4b4062fd4d93730112
Craig Boyles <craigorso@gmail.com>
2014-02-28 21:56:01 -0800

    Added auth formatting and tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,10 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
 end
+
+class ActionController::TestCase
+	include Devise::TestHelpers
+end
+
