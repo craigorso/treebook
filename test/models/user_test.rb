bd5dd70532709872c7d503db19b20f62d53d6df2
Craig Boyles <craigorso@gmail.com>
2014-03-02 11:47:42 -0800

    generated profile controller

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -24,12 +24,24 @@ class UserTest < ActiveSupport::TestCase
 
   test "a user should have a unique profile_name" do
   	user = User.new
-  	#user.profile_name = users(:craig).profile_name
 
-  	user = users(:craig)
-  	user.profile_name = "bad profile"
+    user.profile_name = users(:craig).profile_name
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user can not have spaces in profile name" do
+    user = User.new(first_name: 'craig', last_name: 'xoyles', email: 'bob@gmail.com')
+    user.password = user.password_confirmation = 'asdqweasd'
+    user.profile_name = 'xman 231'
+    assert user.valid?
+  end
+
+  test "a user can have a correcly formatted name" do
+    user = User.new(first_name: 'craig', last_name: 'xoyles', email: 'bob@gmail.com')
+    user.password = user.password_confirmation = 'asdqweasd'
+    user.profile_name = 'xman231'
+    assert user.valid?
   end
 end
